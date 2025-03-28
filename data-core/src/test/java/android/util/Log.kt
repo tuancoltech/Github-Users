bc0d43843f65f309dd58693d2633952d348709e3
Tuan Nguyen <tuancoltech@gmail.com>
2025-03-28 16:32:15 +0700

    Add some basic unit tests for ResponseExt

---
+++ data-core/src/test/java/android/util/Log.kt
@@ -0,0 +1,9 @@
+package android.util
+
+object Log {
+    @JvmStatic fun d(tag: String, msg: String) = 0
+    @JvmStatic fun e(tag: String, msg: String) = 0
+    @JvmStatic fun i(tag: String, msg: String) = 0
+    @JvmStatic fun v(tag: String, msg: String) = 0
+    @JvmStatic fun w(tag: String, msg: String) = 0
+}
